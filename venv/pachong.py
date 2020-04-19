1be682f2ac78a0664c2ec6f1073ea9a2282d8f0c
bronya-so-cute <63329619+bronya-so-cute@users.noreply.github.com>
2020-04-19 18:50:17 +0800

    Update Steamstorecrawler.py
    Add more functions,now you can get many information about discounted games,like name,release date,discount,originalprice,current price,hyperlink to the game's steamstore page,game cover image,etc.
    But there is a bug that if the game don't have information about date,all the information about this game will not be displayed.I think I will try to skip this if the game don't have date,or just delete thiis function.

--- venv/pachong.py
+++ venv/pachong.py
@@ -6,18 +6,19 @@ from bs4 import BeautifulSoup
 url = "https://store.steampowered.com/search/?specials=1&specials=1"+"&os=win"
 s = requests.session()
 res = s.get(url).text
-'''print(res)'''
 soup = BeautifulSoup(res, "html.parser")
 contents = soup.find_all('a',class_='search_result_row ds_collapse_flag')
-print(contents)
 for content in contents:
     try:
         name = content.find(class_="title").string.strip()
-        '''date = content.find("div",class_="col search_released responsive_secondrow").string.strip()'''
-        discount=content.find("span").string.strip()
-        '''price= content.find("div",class_="col search_price discounted responsive_secondrow").string.strip()'''
+        date = content.find("div",class_="col search_released responsive_secondrow").string.strip()
+        discount=content.find("div",class_="col search_discount responsive_secondrow").find('span').string.strip()
+        price=content.find("div",class_="col search_price_discount_combined responsive_secondrow").get("data-price-final")
+        price=int(price)
+        price=price/100
+        Originalprice= content.find("strike").string.strip()
         img_src = content.find("div",class_="col search_capsule").find('img').get("src")
         href=content.get("href")
-        '''print(name,href,discount,img_src+'\n')'''
+        print(name,"/",date,"/",discount,"/",Originalprice,'-> ¥',price,"/",href,"/",img_src+'\n')
     except:
-        print("error")
+        continue
